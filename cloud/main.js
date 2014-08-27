d55b545d0dcef6fce298519528b7304417a31151
Simon Tännler <simontea@edgeguard.ch>
2014-08-27 18:40:45 +0200

    removing routes

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
-var config = require('cloud/config.js'),
-  routes = require('cloud/routes.js');
+var background = require('cloud/background.js'),
+  config = require('cloud/config.js'),
+  lfm = require('cloud/lastFm.js'),
+  spotify = require('cloud/spotify.js'),
+  //routes = require('cloud/routes.js');
+  triggers = require('cloud/triggers.js');
 
-// init routes with config
-routes(config);
+// init with config and lfm
+//routes(config, lfm);
+background(config, lfm, spotify);
+triggers(config, lfm, spotify);
