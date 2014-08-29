b67b4cf1741d97aedeeedf98d180cbd70b114415
Simon Tännler <simontea@edgeguard.ch>
2014-08-29 17:47:04 +0200

    only call when no spotifyId

--- cloud/triggers.js
+++ cloud/triggers.js
@@ -12,13 +12,17 @@ module.exports = function(config, lfm) {
   Parse.Cloud.beforeSave("Artist", function(req, res) {
     var parseArtist = req.object;
 
-    spotify.searchForArtist({
-      q: parseArtist.get("name"),
-      limit: 1
-    }).then(function(httpResponse) {
-      parseArtist.set("spotifyId", httpResponse.data.artists.items[0].id);
-      return spotify.updateTotalAlbumsOfArtist(parseArtist);
-    }).then(res.success, res.error);
+    if (!parseArtist.get("spotifyId")) {
+      spotify.searchForArtist({
+        q: parseArtist.get("name"),
+        limit: 1
+      }).then(function(httpResponse) {
+        parseArtist.set("spotifyId", httpResponse.data.artists.items[0].id);
+        return spotify.updateTotalAlbumsOfArtist(parseArtist);
+      }).then(res.success, res.error);
+    } else {
+      res.success();
+    }
   });
 
 
